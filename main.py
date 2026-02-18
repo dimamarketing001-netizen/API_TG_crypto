f5ddeed65bdde774ac0a9101575bc04058e70cfb
dimamarketing001-netizen <dimamarketing001@gmail.com>
2026-02-18 14:23:48 +0500

    Правки

--- main.py
+++ main.py
@@ -218,7 +218,6 @@ async def handle_calculation(data: CalculationData):
         )
         return {"status": "success"}
     except Exception as e:
-        print(f"Error: {e}")
         raise HTTPException(status_code=500, detail=str(e))
 
 
